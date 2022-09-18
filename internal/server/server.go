50d08f280e9ab739693ca4104c69d806d60bd533
sota_masuda <sota.masuda@the-lionsdata.com>
2022-09-18 23:44:57 +0900

    add server.go / implement Produce, Consume method

---
+++ internal/server/server.go
@@ -0,0 +1,46 @@
+package server
+
+import (
+	"context"
+
+	api "github.com/sota0121/proglog/api/v1"
+)
+
+type Config struct {
+	CommitLog CommitLog
+}
+
+var _ api.LogServer = (*grpcServer)(nil) // grpcServer implements api.LogServer
+
+type grpcServer struct {
+	api.UnimplementedLogServer
+	*Config
+}
+
+type CommitLog interface {
+	Append(*api.Record) (uint64, error)
+	Read(uint64) (*api.Record, error)
+}
+
+func newgrpcServer(config *Config) (srv *grpcServer, err error) {
+	srv = &grpcServer{
+		Config: config,
+	}
+	return srv, nil
+}
+
+func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (*api.ProduceResponse, error) {
+	offset, err := s.CommitLog.Append(req.Record)
+	if err != nil {
+		return nil, err
+	}
+	return &api.ProduceResponse{Offset: offset}, nil
+}
+
+func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (*api.ConsumeResponse, error) {
+	record, err := s.CommitLog.Read(req.Offset)
+	if err != nil {
+		return nil, err
+	}
+	return &api.ConsumeResponse{Record: record}, nil
+}
